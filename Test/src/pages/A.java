3cdc96f80cd1a444debc0f2a100b8aa34d2a7925
atul123 <abharadwajj@gmail.com>
2018-09-03 15:28:12 +0530

    added

---
+++ Test/src/pages/A.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		System.out.println(2);
+	}
+public void test()
+{
+System.out.println(1.1);	
+}
+}
